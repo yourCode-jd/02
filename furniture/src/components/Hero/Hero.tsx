e181500cd3414cb18d903b671c0a860b67c3229f
jaspreet tpss <jaspreetstpss@gmail.com>
2025-01-17 23:42:13 +0530

    banner design

--- furniture/src/components/Hero/Hero.tsx
+++ furniture/src/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ function Hero() {
         <div className="">
             <div className="text-black text-3xl flex items-center justify-center">
                 <Image
-                    src={"/slider-1.png"}
+                    src={"/slider-2.png"}
                     width={1920}
                     height={800}
                     alt='banner slide'
--- furniture/src/components/dropdown/Dropdown.tsx
+++ furniture/src/components/dropdown/Dropdown.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React, { use, useState } from "react";
+import { downArrow } from "../Icons/Icons";
 const Dropdown = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -13,19 +14,7 @@ const Dropdown = (props) => {
         className="inline-flex text-sm justify-center items-center w-full leading-normal border-l border-gray-200 pl-3 text-black"
       >{props.title}
 
-        <svg
-          className="-mr-1 ml-2 h-5 w-5"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            fillRule="evenodd"
-            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
+        {downArrow}
       </button>
 
       {/* Dropdown Menu */}
