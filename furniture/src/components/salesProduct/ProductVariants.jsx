506626e8ef80f1b4e645feb71832e396442fd4da
jaspreet tpss <jaspreetstpss@gmail.com>
2025-01-23 15:12:18 +0530

    sales product hover effect added

--- furniture/src/components/salesProduct/ProductVariants.jsx
+++ furniture/src/components/salesProduct/ProductVariants.jsx
@@ -1,6 +1,6 @@
 import { cart, compare, searchIcon, wishlist } from "../icons/Icons";
-import Image from "next/image";
 
+// Product Color Swatches
 export const Swatches = () => (
   <ul className="flex gap-2 justify-center mt-4">
     <li className="swatchRed"></li>
@@ -8,6 +8,7 @@ export const Swatches = () => (
   </ul>
 );
 
+// Product sizes
 export const ProductSize = () => (
   <ul className="flex gap-2 justify-center mt-4">
     <li className="variantSmile">S</li>
@@ -15,8 +16,9 @@ export const ProductSize = () => (
   </ul>
 );
 
+// Product buttons
 export const ProductBtns = () => (
-  <ul className="flex items-center justify-center gap-4 absolute bottom-44 left-1/2 -translate-x-1/2">
+  <ul className="flex items-center justify-center gap-4 absolute opacity-0 top-0 duration-200 ease-in group-hover:top-4 group-hover:opacity-100 left-1/2 -translate-x-1/2">
     <li className="productBtn">{cart}</li>
     <li className="productBtn">{wishlist}</li>
     <li className="productBtn">{compare}</li>
@@ -24,21 +26,7 @@ export const ProductBtns = () => (
   </ul>
 );
 
-export const ProductItem = ({ index, item }) => (
-  <li className="relative">
-    <Image
-      className=""
-      width={600}
-      height={600}
-      src={item.img}
-      alt={item.alt || "Product image"}
-    />
-    <h3 className="mt-4 text-center text-base text-black tracking-widest">
-      {item.title}
-    </h3>
-    <div className="mt-3 text-center text-sm text-black">$250</div>
-    <Swatches />
-    <ProductSize />
-    <ProductBtns />
-  </li>
-);
+// Product card
+// export const ProductItem = ({ index, item }) => (
+
+// );
--- furniture/src/components/salesProduct/SalesProduct.jsx
+++ furniture/src/components/salesProduct/SalesProduct.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { ProductItem } from "@/components/salesProduct/ProductVariants";
+import Image from "next/image";
+import {
+  ProductSize,
+  Swatches,
+  ProductBtns,
+} from "@/components/salesProduct/ProductVariants";
 import { SalesProductData } from "@/staticDb/data";
 
 function SalesProduct() {
@@ -7,8 +12,23 @@ function SalesProduct() {
     <div className="container">
       <h2 className="main-heading text-center mt-10">Hot Sales !</h2>
       <ul className="grid grid-cols-4 gap-8 mt-12">
-        {SalesProductData.map((item, index) => (
-          <ProductItem index={index} item={item} />
+        {SalesProductData.map((item) => (
+          <li className="relative group">
+            <Image
+              className=""
+              width={600}
+              height={600}
+              src={item.img}
+              alt={item.alt || "Product image"}
+            />
+            <h3 className="mt-4 text-center text-base text-black tracking-widest">
+              {item.title}
+            </h3>
+            <div className="mt-3 text-center text-sm text-black">$250</div>
+            <Swatches />
+            <ProductSize />
+            <ProductBtns />
+          </li>
         ))}
       </ul>
     </div>
