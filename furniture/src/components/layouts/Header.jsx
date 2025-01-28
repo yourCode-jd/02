337859cd9b002b1302fbd5aa7d490fa3a9c638f1
jaspreet tpss <jaspreetstpss@gmail.com>
2025-01-28 23:59:43 +0530

    mega menu design

--- furniture/src/components/layouts/Header.jsx
+++ furniture/src/components/layouts/Header.jsx
@@ -4,74 +4,78 @@ import Link from "next/link";
 import { cart, downArrow, login, searchIcon, wishlist } from "../icons/Icons";
 
 function Header() {
+  const renderSubMenu = (subMenu) =>
+    subMenu?.map((item) => (
+      <li key={item.id} className="relative">
+        <Link href={item.href} className="text-[10px] font-normal inline-block">
+          <img src={item.img} alt={item.label} className="" />
+          <h3 className="text-sm text-black font-medium text-center uppercase my-3 tracking-widest">
+            {item.label}
+          </h3>
+        </Link>
+      </li>
+    ));
+
+  const renderMegaMenu = (megaMenu) =>
+    megaMenu?.map((megaMenuItem) => (
+      <li key={megaMenuItem.id}>
+        <h3 className="text-base font-medium capitalize mb-2 pb-2 border-b border-gray-200 ">
+          {megaMenuItem.label}
+        </h3>
+        <ul>
+          {megaMenuItem.subMenu.map((subMenuItem) => (
+            <li key={subMenuItem.id}>
+              <Link
+                href={subMenuItem.href}
+                className="text-sm font-normal inline-block py-2 text-gray-500 hover:text-black ease-linear duration-150"
+              >
+                {subMenuItem.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </li>
+    ));
+
   return (
-    <div className="container_large">
-      <div className="flex justify-between items-center gap-3 lg:py-6 py-3">
+    <header className="container_large">
+      <div className="flex justify-between items-center gap-3">
         <div className="text-black text-center text-2xl sm:text-3xl capitalize font-medium leading-normal">
           logo
         </div>
-        <ul className=" hidden md:flex xl:ml-20 ml-10 mr-auto items-center gap-6 md:gap-10 lg:gap-12">
+        <nav className="hidden md:flex xl:ml-20 ml-10 mr-auto items-center gap-6 md:gap-10 lg:gap-12">
           {navLinks.map((link) => (
-            <li key={link.key} className="position-relative group">
+            <li key={link.key} className="group list-none">
               <Link
-                className=" flex items-center uppercase text-[12px] font-medium"
                 href={link.href}
+                className="flex items-center uppercase text-[12px] font-medium lg:py-8"
               >
                 {link.label}
                 {downArrow}
               </Link>
-              <div className=" flex absolute top-[130px] z-10 w-2/4 opacity-0 h-0 group-hover:opacity-100 group-hover:h-full duration-300 ease-in origin-top overflow-hidden ">
+              <div className="absolute top-32 z-10 w-2/4 opacity-0 h-0 group-hover:opacity-100 group-hover:h-auto duration-300 ease-in-out overflow-hidden">
                 {link.subMenu && (
-                  <ul className="grid grid-cols-4 gap-x-4 bg-white p-5 pb-0">
-                    {link.subMenu.map((item) => (
-                      <li key={item.id} className="relative">
-                        <Link
-                          href={item.href}
-                          className="text-[10px] font-normal inline-block "
-                        >
-                          <img src={item.img} alt="" className="" />
-                          <h3 className="text-sm text-black font-medium text-center uppercase my-3 tracking-widest">
-                            {item.label}
-                          </h3>
-                        </Link>
-                      </li>
-                    ))}
+                  <ul className="grid grid-cols-2 lg:grid-cols-4 gap-x-4 bg-white p-5 pb-0">
+                    {renderSubMenu(link.subMenu)}
                   </ul>
                 )}
-                {link.megaMenu &&
-                  link.megaMenu.map((megaMenuitem) => (
-                    <ul>
-                      {megaMenuitem.subMenu && (
-                        <li key={megaMenuitem.id}>
-                          <h3>{megaMenuitem.label}</h3>
-                          <ul>
-                            {megaMenuitem.subMenu.map((subMenuitem) => (
-                              <li key={subMenuitem.id}>
-                                <Link
-                                  href={subMenuitem.href}
-                                  className="text-[10px] font-normal inline-block "
-                                >
-                                  {subMenuitem.label}
-                                </Link>
-                              </li>
-                            ))}
-                          </ul>
-                        </li>
-                      )}
-                    </ul>
-                  ))}
+                {link.megaMenu && (
+                  <ul className="bg-white p-5 flex flex-wrap justify-between gap-5">
+                    {renderMegaMenu(link.megaMenu)}
+                  </ul>
+                )}
               </div>
             </li>
           ))}
-        </ul>
-        <div className="flex items-center gap-4 ">
+        </nav>
+        <div className="flex items-center gap-4">
           {searchIcon}
           {login}
           {wishlist}
           {cart}
         </div>
       </div>
-    </div>
+    </header>
   );
 }
 
--- furniture/src/components/navLinks/index.ts
+++ furniture/src/components/navLinks/index.ts
@@ -1,6 +1,4 @@
 // navigation
-
-
 export const navLinks = [
     { 
         href: "/", key: "home", label: "home",
@@ -9,16 +7,45 @@ export const navLinks = [
             { id:"2", href: "/", img: "/product01.png", label: "product 2" },
             { id:"3", href: "/", img: "/product01.png", label: "product 3" },
             { id:"4", href: "/", img: "/product01.png", label: "product 4" },
-            // { id:"5", href: "/", img: "/product01.png", label: "product 5" },
-            // { id:"6", href: "/", img: "/product01.png", label: "product 6" },
-            // { id:"7", href: "/", img: "/product01.png", label: "product 7" },
-            // { id:"8", href: "/", img: "/product01.png", label: "product 8" },
+            { id:"5", href: "/", img: "/product01.png", label: "product 5" },
+            { id:"6", href: "/", img: "/product01.png", label: "product 6" },
+            { id:"7", href: "/", img: "/product01.png", label: "product 7" },
+            { id:"8", href: "/", img: "/product01.png", label: "product 8" },
         ]
 
     },
     { href: "/", key: "shop",   label: "shop",
         megaMenu: [
-            {  id:"1", href: "/", label: "Layout",
+            {  id:"1", href: "/", label: "Logbook",
+                subMenu:[
+                    { id:"1", href: "/", label: "Standard" },   
+                    { id:"2", href: "/", label: "Simple" },
+                    { id:"3", href: "/", label: "Overlay" },
+                    { id:"4", href: "/", label: "Overlay Side" },
+                    { id:"5", href: "/", label: "Overlay Side New" },
+                    { id:"6", href: "/", label: "Without Sidebar" },    
+            ]                               
+            },
+            {  id:"2", href: "/", label: "Blog",
+                subMenu:[
+                    { id:"1", href: "/", label: "Blog Classic" },
+                    { id:"2", href: "/", label: "Blog Grid" },
+                    { id:"3", href: "/", label: "Blog Masonry" },                    
+            ]                               
+            }, 
+            {  id:"3", href: "/", label: "Pages",
+                subMenu:[
+                    { id:"1", href: "/", label: "About" },
+                    { id:"2", href: "/", label: "Contact" },
+                    { id:"3", href: "/", label: "Cart" },
+                    { id:"4", href: "/", label: "Checkout" },
+                    { id:"5", href: "/", label: "Compare" },
+                    { id:"6", href: "/", label: "Wishlist" },
+                    { id:"7", href: "/", label: "My Account" },
+                    { id:"8", href: "/", label: "Order Complete" },                    
+            ]                               
+            },                            
+            {  id:"4", href: "/", label: "portfolio",
                 subMenu:[
                     { id:"1", href: "/", label: "Standard" },
                     { id:"2", href: "/", label: "Standard With Banner" },
@@ -32,6 +59,21 @@ export const navLinks = [
                     { id:"10", href: "/", label: "Collection List 1" },
                     { id:"11", href: "/", label: "Collection List 2" },
                     { id:"12", href: "/", label: "Collection SliderNew" },
+            ]
+            },
+                ],
+    },
+    { href: "/", key: "product",   label: "product",    
+        megaMenu: [
+            {  id:"1", href: "/", label: "Layout",
+                subMenu:[
+                    { id:"1", href: "/", label: "Standard" },
+                    { id:"2", href: "/", label: "Standard With Banner" },
+                    { id:"3", href: "/", label: "Categories Image 1" },
+                    { id:"4", href: "/", label: "Categories Image 2" },
+                    { id:"5", href: "/", label: "Fullwidth" },
+                    { id:"6", href: "/", label: "List view" },
+                    { id:"7", href: "/", label: "Simple" },
             ]
             },
             {  id:"2", href: "/", label: "Product",
@@ -65,7 +107,107 @@ export const navLinks = [
             }, 
                 ],
     },
-    { href: "/", key: "product",    label: "product" },
-    { href: "/", key: "blog",    label: "blog" },
-    { href: "/", key: "feature", label: "feature" },
+
+    { href: "/", key: "blog",   label: "blog",
+        megaMenu: [
+            {  id:"1", href: "/", label: "filter",
+                subMenu:[
+                    { id:"1", href: "/", label: "Standard" },
+                    { id:"2", href: "/", label: "Standard With Banner" },
+                    { id:"3", href: "/", label: "Categories Image 1" },
+                    { id:"4", href: "/", label: "Categories Image 2" },
+                    { id:"5", href: "/", label: "Fullwidth" },
+                    { id:"6", href: "/", label: "List view" },
+                    { id:"7", href: "/", label: "Simple" },
+                    { id:"8", href: "/", label: "MansonryHot" },
+                    { id:"9", href: "/", label: "Overlay Header" },
+                    { id:"10", href: "/", label: "Collection List 1" },
+                    { id:"11", href: "/", label: "Collection List 2" },
+                    { id:"12", href: "/", label: "Collection SliderNew" },
+            ]
+            },
+            {  id:"2", href: "/", label: "Blog",
+                subMenu:[
+                    { id:"1", href: "/", label: "Blog Classic" },
+                    { id:"2", href: "/", label: "Blog Grid" },
+                    { id:"3", href: "/", label: "Blog Masonry" },                    
+            ]                               
+            }, 
+            {  id:"3", href: "/", label: "Pages",
+                subMenu:[
+                    { id:"1", href: "/", label: "About" },
+                    { id:"2", href: "/", label: "Contact" },
+                    { id:"3", href: "/", label: "Cart" },
+                    { id:"4", href: "/", label: "Checkout" },
+                    { id:"5", href: "/", label: "Compare" },
+                    { id:"6", href: "/", label: "Wishlist" },
+                    { id:"7", href: "/", label: "My Account" },
+                    { id:"8", href: "/", label: "Order Complete" },                    
+            ]                               
+            }, 
+            {  id:"4", href: "/", label: "loaded & cart",
+                subMenu:[
+                    { id:"1", href: "/", label: "Standard" },   
+                    { id:"2", href: "/", label: "Simple" },
+                    { id:"3", href: "/", label: "Overlay" },
+                    { id:"4", href: "/", label: "Overlay Side" },
+                    { id:"5", href: "/", label: "Overlay Side New" },
+                    { id:"6", href: "/", label: "Without Sidebar" },    
+            ]                               
+            },
+                                       
+            
+                ],
+    },
+
+    { href: "/", key: "feature",   label: "feature",
+        megaMenu: [
+            {  id:"1", href: "/", label: "Logbook",
+                subMenu:[
+                    { id:"1", href: "/", label: "Standard" },   
+                    { id:"2", href: "/", label: "Simple" },
+                    { id:"3", href: "/", label: "Overlay" },
+                    { id:"4", href: "/", label: "Overlay Side" },
+                    { id:"5", href: "/", label: "Overlay Side New" },
+                    { id:"6", href: "/", label: "Without Sidebar" },    
+            ]                               
+            },
+            {  id:"2", href: "/", label: "Blog",
+                subMenu:[
+                    { id:"1", href: "/", label: "Blog Classic" },
+                    { id:"2", href: "/", label: "Blog Grid" },
+                    { id:"3", href: "/", label: "Blog Masonry" },                    
+            ]                               
+            }, 
+            {  id:"3", href: "/", label: "Pages",
+                subMenu:[
+                    { id:"1", href: "/", label: "About" },
+                    { id:"2", href: "/", label: "Contact" },
+                    { id:"3", href: "/", label: "Cart" },
+                    { id:"4", href: "/", label: "Checkout" },
+                    { id:"5", href: "/", label: "Compare" },
+                    { id:"6", href: "/", label: "Wishlist" },
+                    { id:"7", href: "/", label: "My Account" },
+                    { id:"8", href: "/", label: "Order Complete" },                    
+            ]                               
+            },                            
+            {  id:"4", href: "/", label: "portfolio",
+                subMenu:[
+                    { id:"1", href: "/", label: "Standard" },
+                    { id:"2", href: "/", label: "Standard With Banner" },
+                    { id:"3", href: "/", label: "Categories Image 1" },
+                    { id:"4", href: "/", label: "Categories Image 2" },
+                    { id:"5", href: "/", label: "Fullwidth" },
+                    { id:"6", href: "/", label: "List view" },
+                    { id:"7", href: "/", label: "Simple" },
+                    { id:"8", href: "/", label: "MansonryHot" },
+                    { id:"9", href: "/", label: "Overlay Header" },
+                    { id:"10", href: "/", label: "Collection List 1" },
+                    { id:"11", href: "/", label: "Collection List 2" },
+                    { id:"12", href: "/", label: "Collection SliderNew" },
+            ]
+            },
+                ],
+    },
+
 ]
